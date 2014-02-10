7e6171fb4ca3a9d8f80040d4ea7c4738891e5437
erramun <ralonso@kiro-robotics.com>
2014-02-10 12:56:54 +0100

    new feature 2

--- vs-tester/Controllers/HomeController.vb
+++ vs-tester/Controllers/HomeController.vb
@@ -8,7 +8,7 @@
     End Function
 
     Function About() As ActionResult
-        ViewData("Message") = "new feature one."
+        ViewData("Message") = "new feature two."
 
         Return View()
     End Function
